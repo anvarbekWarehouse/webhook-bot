09b610e322a0b2e71e2a81750bc2d4f8e9b2e80f
anvarbekWarehouse <130168718+anvarbekWarehouse@users.noreply.github.com>
2024-04-25 21:47:12 +0500

    Schdeluer
    Schdeluer

--- src/main/java/uz/hamkorbank/appwebhooktelegrammbot/controller/ServerController.java
+++ src/main/java/uz/hamkorbank/appwebhooktelegrammbot/controller/ServerController.java
@@ -1,6 +1,7 @@
 package uz.hamkorbank.appwebhooktelegrammbot.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import uz.hamkorbank.appwebhooktelegrammbot.service.ServerService;
 @RestController
 @RequestMapping("/api/server")
 @RequiredArgsConstructor
+@Slf4j
 public class ServerController {
     private final ServerService serverService;
 
@@ -27,8 +29,9 @@ public class ServerController {
         return serverService.deleteById(id);
     }
 
-   @Scheduled(fixedRate = 3600000)
+   @Scheduled(fixedRate = 3000)
     public void scheduledServer(){
+         log.info("Job running: "+ new Date());
         serverService.scheduledServer();
    }
 
